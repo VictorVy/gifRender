cc9309cfe6d483f277eb15118d38f42698ecdaca
Victor Yao <victoryao1039@gmail.com>
2023-02-08 10:21:26 -0800

    refactor Roster field

--- src/main/model/Roster.java
+++ src/main/model/Roster.java
@@ -6,54 +6,54 @@ import java.util.ArrayList;
 
 // Represents the collection of frames comprising the output gif
 public class Roster {
-    ArrayList<RosterItem> roster;
+    ArrayList<RosterItem> items;
 
     public Roster() {
-        roster = new ArrayList<>();
+        items = new ArrayList<>();
     }
 
     // MODIFIES: this
     // EFFECTS: adds ri to the roster
     public void add(RosterItem ri) {
-        roster.add(ri);
+        items.add(ri);
     }
 
     // REQUIRES: 0 <= i < size();
     // MODIFIES: this
     // EFFECTS: removes the RosterItem at index i from the roster
     public void remove(int i) {
-        roster.remove(i);
+        items.remove(i);
     }
 
     // MODIFIES: this
     // EFFECTS: clears the roster
     public void clear() {
-        roster.clear();
+        items.clear();
     }
 
     // EFFECTS: returns true if the roster is empty
     public boolean isEmpty() {
-        return roster.isEmpty();
+        return items.isEmpty();
     }
 
     // EFFECTS: returns the size of the roster
     public int size() {
-        return roster.size();
+        return items.size();
     }
 
     // REQUIRES: 0 <= i < size();
     // EFFECTS: returns the RosterItem at index i
     public RosterItem getItem(int i) {
-        return roster.get(i);
+        return items.get(i);
     }
 
     // EFFECTS: returns the roster
     public ArrayList<RosterItem> getItems() {
-        return roster;
+        return items;
     }
 
     // EFFECTS: returns the roster as an array of GIFFrames
     public GIFFrame[] getFrames() {
-        return ModelUtils.toGifFrames(roster);
+        return ModelUtils.toGifFrames(items);
     }
 }
